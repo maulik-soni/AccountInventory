82cce355edb7cb950df03fe24db42f1d4e4945b6
jeet10214 <jeet10214@gmail.com>
2017-06-03 02:14:34 +0530

    initial setup for Memo APIs

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/memoin', "memoinController@memoinEntry");
+Route::post('/memoout', "memooutController@memooutEntry");
 
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {*/
     /*return $request->user();*/
