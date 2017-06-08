b62138920afbca99ba2863888e0cb5e54c9be189
sonimaulik <mauliksoni37@gmail.com>
2017-06-08 22:06:34 +0530

    adding migrations files for the tables

---
+++ api/database/migrations/2017_06_08_162913_create_memo_out_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMemoOutTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('memo_out', function(Blueprint $table)
+		{
+			$table->integer('PCS_ID')->primary();
+			$table->string('memo_invoice_number', 256)->nullable();
+			$table->string('data', 256)->nullable();
+			$table->string('account_name', 256)->nullable();
+			$table->string('broker', 256)->nullable();
+			$table->string('reference', 256)->nullable();
+			$table->float('carats', 10, 0);
+			$table->integer('pending_pcs');
+			$table->float('pending_carats', 10, 0);
+			$table->float('amount', 10, 0);
+			$table->string('stone_type', 256)->nullable();
+			$table->integer('no_of_days');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('memo_out');
+	}
+
+}
