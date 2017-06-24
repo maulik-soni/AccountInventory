5b1a561e499021294e02d4295b89c682872014c9
Vikas <vikas.p.bewdigital@gmail.com>
2017-06-24 22:56:54 +0530

    logout issue solved

--- app/src/app/dashboard-structure/dashboard-structure/dashboard-structure.component.ts
+++ app/src/app/dashboard-structure/dashboard-structure/dashboard-structure.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { AuthService } from './../../authorization/auth.service';
 
 @Component({
   selector:'dashboard',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-structure.component.css']
 })
 export class DashboardStructureComponent implements OnInit {
-
-  constructor() { }
+   isAdmin=false;
+  constructor(
+     private route:Router,
+              private _auth:AuthService
+  ) { }
 
   ngOnInit() {
+     if(this._auth.HasRole()=='admin'){
+      this.isAdmin=true;
+    }
+  }
+
+  logout(){
+            localStorage.clear();
+            this.route.navigate(['/login']);
+
   }
 
 }
